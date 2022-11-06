cef8df38a7cea6e89e96e712697d4b633d86023f
louiskwon <louiskwon@gmial.com>
2022-11-06 21:45:33 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경
    #3은 여기서 마무리
    스프링으로 넘어감

--- src/main/java/com/fastcampus/Main.java
+++ src/main/java/com/fastcampus/Main.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Sort<String> sort = new JavaSort<>();
+        BubbleSort<String> sort = new BubbleSort<>();
+        //Sort<String> sort = new JavaSort<>();
 
         System.out.println("[result]" + sort.sort(Arrays.asList(args)));
     }
